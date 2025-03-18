a1de476a653c09e1014413462910a41e6b7f48ee
Catalin Lazar <lazar.florin.catalin02@gmail.com>
2025-03-18 13:39:34 +0200

    Add image processing alg: contours, SIFT and ORB.

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ import shutil
 
 import pandas as pd
 from src.logo_fetch import scrape_website_logo
+from src.image_processing import image_resizing, image_contour_processing, image_SIFT_processing, image_ORB_processing
 sys.path.append('src')
 
 if __name__ == '__main__':
-	# read .parquet file
-	df = pd.read_parquet('src/dataset/logos.snappy.parquet', engine='pyarrow')
-	print(df.values)
-
-	# store websites into a list
-	websites = df.values.tolist()
-
-	# iterate list of websites
-	for website in websites[:20]:
-		scrape_website_logo(website[0])
+	# websites scraping
+	scrape_website_logo()
 	print('Websites scraped')
+
+	# process images
+	image_resizing()
+	image_contour_processing()
+	image_SIFT_processing()
+	image_ORB_processing()
 
 	# remove logos folder
 	if os.path.exists('src/logos'):
 		print('Folder exists')
 		shutil.rmtree('src/logos')
+		print('Removing folder')
+
+	if os.path.exists('src/resized_logos'):
+		print('Folder exists')
+		shutil.rmtree('src/resized_logos')
 		print('Removing folder')
--- src/logo_fetch.py
+++ src/logo_fetch.py
@@ -1,6 +1,7 @@
 import os
 import urllib3
-from urllib.parse import urljoin
+import pandas as pd
+from urllib.parse import urljoin, urlparse
 from bs4 import BeautifulSoup as bs
 
 '''
@@ -25,62 +26,83 @@ COMMON_WORDS = ['logo', 'brand', 'site-logo']
 Function for searching and downloading 
 websites logo.
 '''
-def scrape_website_logo(url):
-	# split url to get website name
-	website_name = url.split('.')[0]
 
-	try:
-		# fetch website response
-		response = http.request('GET', url, timeout=1)
+# Return a valid URL
+def ensure_http(url):
+	if not url.startswith(('http://', 'https://')):
+		return 'https://' + url
+	return url
 
-		# html parse
-		soup = bs(response.data, 'html.parser')
+# Retrieve websites from parquet file
+def retrieve_websites():
+	# read .parquet file
+	df = pd.read_parquet('src/dataset/logos.snappy.parquet', engine='pyarrow')
 
-		# look-up for logo in html parse data
-		for img in soup.find_all('img'):
-			# print(img)
+	# store websites into a list
+	websites = [ensure_http(w[0]) for w in df.values.tolist() if w[0]]
+	return websites
 
-			'''
-			Search for attributes: src, class or id
-			and check for each of the common keywords
-			'''
-			try:
-				src = img.get('src', '')
-				if src:
-					if any(keyword in src.lower() for keyword in COMMON_WORDS) or \
-							any(keyword in img.get('class', []) for keyword in COMMON_WORDS) or \
-							any(keyword in img.get('id', '') for keyword in COMMON_WORDS):
+'''
+	Scraping website for logo and save the image into a folder
+'''
+def scrape_website_logo():
+	# iterate list of websites links
+	for url in retrieve_websites()[:20]:
+		# split url to get website name
+		website_name = urlparse(url).netloc.split('.')[0]
 
-						logo_url = urljoin(url, src)
-						# print(f'Logo: {logo_url}')
+		try:
+			# fetch website response
+			response = http.request('GET', url, timeout=1)
 
-						# download the logo
-						try:
-							# fetch response about image url
-							logo_response = http.request('GET', logo_url, timeout=1)
-							# verify response
-							if logo_response.status == 200:
-								# give file path
-								file_path = os.path.join('src/logos', f'{website_name}.jpg')
-								# save file
-								with open(file_path, 'wb') as logo_file:
-									logo_file.write(logo_response.data)
+			# html parse
+			soup = bs(response.data, 'html.parser')
 
-							else:
-								print('Failed to download logo')
+			# look-up for logo in html parse data
+			for img in soup.find_all('img'):
+				# print(img)
 
-						except Exception as e:
-							print(f'Failed to download logo: {e}')
-						break
+				'''
+				Search for attributes: src, class or id
+				and check for each of the common keywords
+				'''
+				try:
+					src = img.get('src', '')
+					if src:
+						if any(keyword in src.lower() for keyword in COMMON_WORDS) or \
+								any(keyword in img.get('class', []) for keyword in COMMON_WORDS) or \
+								any(keyword in img.get('id', '') for keyword in COMMON_WORDS):
 
-			except Exception as e:
-				print(f'Failed to parse img: {e}')
+							logo_url = urljoin(url, src)
+							# print(f'Logo: {logo_url}')
 
-	except urllib3.exceptions.TimeoutError as e:
-		print(f"TimeoutError for {url}: {e}")
-	except urllib3.exceptions.ConnectionError as e:
-		print(f"ConnectionError for {url}: {e}")
-	except urllib3.exceptions.HTTPError as e:
-		print(f"HTTPError for {url}: {e}")
-	except Exception as e:
-		print(f"Unexpected error for {url}: {e}")
+							# download the logo
+							try:
+								# fetch response about image url
+								logo_response = http.request('GET', logo_url, timeout=1)
+								# verify response
+								if logo_response.status == 200:
+									# give file path
+									file_path = os.path.join('src/logos', f'{website_name}.jpg')
+									# save file
+									with open(file_path, 'wb') as logo_file:
+										logo_file.write(logo_response.data)
+
+								else:
+									print('Failed to download logo')
+
+							except Exception as e:
+								print(f'Failed to download logo: {e}')
+							break
+
+				except Exception as e:
+					print(f'Failed to parse img: {e}')
+
+		except urllib3.exceptions.TimeoutError as e:
+			print(f"TimeoutError for {url}: {e}")
+		except urllib3.exceptions.ConnectionError as e:
+			print(f"ConnectionError for {url}: {e}")
+		except urllib3.exceptions.HTTPError as e:
+			print(f"HTTPError for {url}: {e}")
+		except Exception as e:
+			print(f"Unexpected error for {url}: {e}")
